94ed1d7a51079f583b26733dc4c78e96826e3f69
Aaron Mavrinac <mavrinac@gmail.com>
2009-01-20 00:09:54 -0500

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = "daneel-ai",
+    version = "0.0.1",
+    license = "GPL",
+    description = "An advanced rule based AI for Thousand Parsec.",
+    long_description = "",
+    author = "Vincent Verhoeven",
+    author_email = "verhoevenv@gmail.com",
+    url = "http://www.thousandparsec.net",
+    scripts = ["daneel-ai"],
+    packages = ['.'],
+    data_files = [(".", ("LICENSE", "COPYING", "README")),
+                  ("rules", ("rfts", "risk"))],
+)
