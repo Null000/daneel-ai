8f1c7b36a3f7c5a5d6bb2a4e3b089664e2b320b8
Tim Ansell <mithro@mithis.com>
2009-03-15 13:57:59 +1100

    Added submodules and a requirements.py to daneel-ai.
    This should mean people only have to checkout the daneel-ai directory
    and everything should just work.

---
+++ requirements.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python
+
+# Preference the local directory first...
+import sys
+sys.path.insert(0, '.')
+import os.path
+
+# If we are working from git, initialise the submodules
+modules = [("libtpproto-py", "tp.netlib"), ("libtpclient-py", "tp.client")] #, ("schemepy", "schemepy")]
+if os.path.exists(".git"):
+	for dir, name in modules:
+		if os.path.exists(dir) and not os.path.exists(os.path.join(dir, ".git")):
+			break
+	else:
+		os.system("git submodule init")
+	os.system("git submodule update")
+
+for dir, name in modules:
+	if os.path.exists(dir):
+		sys.path.insert(0, dir)
+
+# Check for our dependencies.
+notfound = []
+for dir, name in modules:
+	try:
+		exec("import %s as module" % name)
+
+		print "%s version" % dir, module.__version__ 
+		print "    (installed at %s)" % module.__installpath__
+
+		try:
+			exec("from %s.version import version_git" % name)
+			print "    (git %s)" % version_git
+		except ImportError:
+			pass
+
+	except (ImportError, KeyError, AttributeError), e:
+		print e
+		notfound.append(dir)
+	print
+
+if len(notfound) > 0:
+	print "The following requirements where not met:"
+	for module in notfound:
+		print '    ', module
+	print
+
+if len(notfound) > 0:
+	import sys
+	sys.exit(1)
+
